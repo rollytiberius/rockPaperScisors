6b90336e5b7cd3f231f71d113347491c81441650
rollytiberius <rollytiberius@gmail.com>
2024-11-11 20:45:21 +0000

    Adding some functions in script js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-console.log("Hello Rolande");
-console.log("Salut Rolande");
+// Here the computer chioce
+
+const choiceArray = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  const randNumber = Math.floor(Math.random() * 3);
+  return choiceArray[randNumber];
+}
+
+console.log(getComputerChoice());
+
+// Get the human chioce
+
+function getHumanChoice() {
+  const humanChoice = prompt(
+    "You need to choice 'rock', 'paper' or 'scissoers'",
+  );
+  return humanChoice;
+}
+
+console.log(getHumanChoice());
+
+// Score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if humanChoice 
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
