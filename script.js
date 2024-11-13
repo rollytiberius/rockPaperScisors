066da94e185d0e33df04e1a8fbf745b41da72ff6
rollytiberius <rollytiberius@gmail.com>
2024-11-13 07:29:50 +0000

    added functional to script

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 // Get the human chioce
-
 function getHumanChoice() {
   const humanChoice = prompt(
     "You need to choice 'rock', 'paper' or 'scissoers'",
   );
-  return humanChoice;
+  return humanChoice.toLowerCase();
 }
 
 console.log(getHumanChoice());
@@ -26,7 +25,33 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  if humanChoice 
+  if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("Drow round... Nobudy Win!!!");
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("Drow round... Nobudy Win!!!");
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    console.log("Drow round... Nobudy Win!!!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("The player wins! Rock beat the Scissors");
+    humanScore += 1;
+    console.log(
+      `Player score = ${humanScore} Computer score = ${computerScore}`,
+    );
+  }
+} else if (humanChoice === "paper" && computerChoice === "rock") {
+  console.log("The player wins! Paper beat the Rock");
+  humanScore += 1;
+  console.log(
+    `Player score = ${humanScore} Computer score = ${computerScore}`,
+  );
+}
+	} else if (humanChoice === "scissors" && computerChoice === "paper") {
+  console.log("The player wins! Scissors beat the Paper");
+  humanScore += 1;
+  console.log(
+    `Player score = ${humanScore} Computer score = ${computerScore}`,
+  );
+}
 }
 
 const humanSelection = getHumanChoice();
