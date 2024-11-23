c18769f9adca15790c6b55d3f56985ca1b917f34
rollytiberius <rollytiberius@gmail.com>
2024-11-23 10:43:02 +0000

    Finish The project

--- script.js
+++ script.js
@@ -16,96 +16,111 @@ function getComputerChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function checkScores() {
-  if (humanScore === 5) {
-    result.textContent = "Player Wins the game";
-    result.textContent +=
-      `Player score ${humanScore} Computer score ${computerScore}.`;
-  }
-}
-
 function playRound(humanChoice, computerChoice) {
-  checkScores();
   if (humanChoice === "rock" && computerChoice === "rock") {
-    result.innerText += "Drow round... Nobudy Win!!!";
+    result.innerText = "Drow round... Nobudy Win!!!";
     result.innerHTML += "<br>";
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (humanChoice === "paper" && computerChoice === "paper") {
-    result.innerText += "Drow round... Nobudy Win!!!";
+    result.innerText = "Drow round... Nobudy Win!!!";
     result.innerHTML += "<br>";
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    result.innerText += "Drow round... Nobudy Win!!!";
+    result.innerText = "Drow round... Nobudy Win!!!";
     result.innerHTML += "<br>";
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    result.innerText += "The player wins! Rock beat the Scissors";
+    result.innerText = "The player wins! Rock beat the Scissors";
     result.innerHTML += "<br>";
     humanScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    result.innerText += "The player wins! Paper beat the Rock";
+    result.innerText = "The player wins! Paper beat the Rock";
     result.innerHTML += "<br>";
     humanScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    result.innerText += "The player wins! Scissors beat the Paper";
+    result.innerText = "The player wins! Scissors beat the Paper";
     result.innerHTML += "<br>";
     humanScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (computerChoice === "rock" && humanChoice === "scissors") {
-    result.innerText += "The player wins! Rock beat the Scissors";
+    result.innerText = "The player wins! Rock beat the Scissors";
     result.innerHTML += "<br>";
     computerScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (computerChoice === "paper" && humanChoice === "rock") {
-    result.innerText += "The computer wins! Paper beat the Rock";
+    result.innerText = "The computer wins! Paper beat the Rock";
     result.innerHTML += "<br>";
     computerScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   } else if (computerChoice === "scissors" && humanChoice === "paper") {
-    result.innerText += "The computer wins! Scissors beat the Paper";
+    result.innerText = "The computer wins! Scissors beat the Paper";
     result.innerHTML += "<br>";
     computerScore += 1;
     result.innerText +=
       `Player score = ${humanScore} Computer score = ${computerScore}`;
-    result.innerHTML += "<br>";
-    result.innerHTML += "<br>";
   }
 }
 
 rockBtn.addEventListener("click", () => {
-  playRound("rock", getComputerChoice());
+  if (computerScore === 5) {
+    alert("Computer Win The Game");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else if (humanScore === 5) {
+    alert("Player Win The GAme");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else {
+    playRound("rock", getComputerChoice());
+  }
 });
 
 paperBtn.addEventListener("click", () => {
-  playRound("paper", getComputerChoice());
+  if (computerScore === 5) {
+    alert("Computer Win The Game");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else if (humanScore === 5) {
+    alert("Player Win The GAme");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else {
+    playRound("paper", getComputerChoice());
+  }
 });
 
 scissorsBtn.addEventListener("click", () => {
-  playRound("scissors", getComputerChoice());
+  if (computerScore === 5) {
+    alert("Computer Win The Game");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else if (humanScore === 5) {
+    alert("Player Win The GAme");
+    result.textContent = "";
+    computerScore = 0;
+    humanScore = 0;
+    return;
+  } else {
+    playRound("scissors", getComputerChoice());
+  }
 });
