de3a47c96a7bc547896c819082e4b3c86567ee34
rollytiberius <rollytiberius@gmail.com>
2024-11-23 12:39:03 +0000

    Revert "Add some function"
    This reverts commit 76944623531a0e2efa013181e462e01bbb1f13f3, reversing
    changes made to c18769f9adca15790c6b55d3f56985ca1b917f34.

--- script.js
+++ script.js
@@ -7,95 +7,16 @@ const scissorsBtn = document.getElementById("scissors");
 const result = document.querySelector(".result");
 
 function getComputerChoice() {
-	const randNumber = Math.floor(Math.random() * 3);
-	return choiceArray[randNumber];
+  const randNumber = Math.floor(Math.random() * 3);
+  return choiceArray[randNumber];
 }
 
-<<<<<<< HEAD
-// Get the human chioce
-function getHumanChoice() {
-	const humanChoice = prompt(
-		"You need to choice 'rock', 'paper' or 'scissoers'",
-	);
-	return humanChoice.toLowerCase();
-}
-
-=======
->>>>>>> rps-ui
 // Score variables
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-<<<<<<< HEAD
-	if (humanChoice === "rock" && computerChoice === "rock") {
-		console.log("Drow round... Nobudy Win!!!");
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (humanChoice === "paper" && computerChoice === "paper") {
-		console.log("Drow round... Nobudy Win!!!");
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (humanChoice === "scissors" && computerChoice === "scissors") {
-		console.log("Drow round... Nobudy Win!!!");
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (humanChoice === "rock" && computerChoice === "scissors") {
-		console.log("The player wins! Rock beat the Scissors");
-		humanScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (humanChoice === "paper" && computerChoice === "rock") {
-		console.log("The player wins! Paper beat the Rock");
-		humanScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (humanChoice === "scissors" && computerChoice === "paper") {
-		console.log("The player wins! Scissors beat the Paper");
-		humanScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (computerChoice === "rock" && humanChoice === "scissors") {
-		console.log("The player wins! Rock beat the Scissors");
-		computerScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (computerChoice === "paper" && humanChoice === "rock") {
-		console.log("The computer wins! Paper beat the Rock");
-		computerScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	} else if (computerChoice === "scissors" && humanChoice === "paper") {
-		console.log("The computer wins! Scissors beat the Paper");
-		computerScore += 1;
-		console.log(
-			`Player score = ${humanScore} Computer score = ${computerScore}`,
-		);
-	}
-}
-
-while (true) {
-	if (computerScore === 3) {
-		console.log("Computer Wins ");
-		break;
-	}
-	if (humanScore === 3) {
-		console.log("Player Wins ");
-		break;
-	} else {
-		playRound(getHumanChoice(), getComputerChoice());
-	}
-}
-=======
   if (humanChoice === "rock" && computerChoice === "rock") {
     result.innerText = "Drow round... Nobudy Win!!!";
     result.innerHTML += "<br>";
@@ -203,4 +124,3 @@ scissorsBtn.addEventListener("click", () => {
     playRound("scissors", getComputerChoice());
   }
 });
->>>>>>> rps-ui
